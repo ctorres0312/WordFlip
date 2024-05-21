233bc4b7ddae51499ad170c5e60b3f5bbf35e7d0
ctorres0312 <113136320+ctorres0312@users.noreply.github.com>
2024-05-21 18:37:56 -0400

    WordFlip v1

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+//Get the String from User Input
+//Controller function
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+//Logic Function
+function reverseString(userString){
+    let revString = [];
+
+    //reverse a string user a for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+    
+    return revString;
+}
+
+//Display the reversed string to the user
+//View Function
+function displayString(revstring){
+
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revstring}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
